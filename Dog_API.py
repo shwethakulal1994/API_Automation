506aceaf9a603fac0b7c03e5dd568e0bbd31d0be
shwethakulal1994 <65589497+shwethakulal1994@users.noreply.github.com>
2024-05-02 10:20:26 +0530

    Updated the Dog_API.py with comments

--- Dog_API.py
+++ Dog_API.py
@@ -1,15 +1,24 @@
-import json
-
 import requests
 get_all_breed = "https://dog.ceo/api/breeds/list/all"
 
+
+#Api get function will retrieve the information
 response = requests.get(get_all_breed)
-print(response.status_code)
+
+#to validate whether status code is 200 or not
 assert response.status_code == 200
-all_val = response.json()
-# print(all_val)
-for dog_breed in all_val['message']:
-    print(dog_breed)
-# all_val = json.dump(response)
-# print(all_val)
-# assert
+
+#.json() will convert the reponse to json format
+print(response.json()['status'])
+
+print("individual breeds are", '\n')
+print("-------------------------------------------------------------------------", '\n')
+
+# For loop will get each breed from all the list of breed
+for dog_breed in response.json()['message']:
+
+    breed= response.json()['message'][dog_breed]
+
+    #if values of breed key is <= 1 then it will be considered as individual dof breed
+    if len(breed) <= 1:
+        print(dog_breed, "- breed count: ", len(breed))
